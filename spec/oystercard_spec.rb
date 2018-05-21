1bf69e3d20f9b823e16fa0a78021b58736e4c829
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-05-21 17:20:33 +0100

    added a test for deducting the balance and wrote a method to pass it. Removed redundant tests using respond_to

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,8 +10,6 @@ describe Oystercard do
   end
 
   context "#top_up" do
-    it { is_expected.to respond_to(:top_up).with(1).argument }
-
     it 'can top up the balance' do
       expect { subject.top_up 1 }.to change{ subject.balance }.by 1
     end
@@ -24,8 +22,11 @@ describe Oystercard do
   end
 
   context "#deduct" do
-    it { is_expected.to respond_to(:deduct).with(1).argument }
-  end
+    it 'deducts an amount from the balance' do
+      subject.top_up(20)
+      expect { subject.deduct 3}.to change{ subject.balance }.by -3
+    end
 
+  end
 
 end
