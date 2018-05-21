4188739edb94344f2c47640145aee5b8b3fadaaf
Chris Russell <chris.john.russell@gmail.com>
2018-05-21 15:38:43 +0100

    added balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
-describe Oystercard do
+require 'oystercard'
 
+describe Oystercard do
+  context "#balance" do
+    it "Return the current balance" do
+    card = Oystercard.new
+    card.balance
+    expect(card.balance).to be_an Integer
+  end
+end
 end
