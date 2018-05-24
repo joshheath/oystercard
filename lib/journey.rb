d2b3c0b88a597e9a75eb8f90f7fa70f702504fc9
diego fontecilla <mail@diegofontecilla.com>
2018-05-24 16:49:02 +0100

    end chapter 14

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,15 +1,26 @@
 class Journey
-
 PENALTY_FARE = 20
+attr_reader :entry_station
+
+def initialize(entry_station = "Dalston")
+  @entry_station = entry_station
+end
+
 
 def complete?
+  @entry_station != nil && @other_station != nil
 end
 
 def fare
+  1
+end
+
+def penalty_fare
   PENALTY_FARE
 end
 
 def finish(station)
+  @other_station = station
   self
 end
 
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -8,11 +8,38 @@ describe Journey do
   end
 
   it 'has a penalty fare by default' do
-    expect(subject.fare).to eq Journey::PENALTY_FARE
+    expect(subject.penalty_fare).to eq Journey::PENALTY_FARE
   end
 
   it 'returns itself when exiting a journey' do
     expect(subject.finish(station)).to eq(subject)
   end
 
+  context 'given an entry station' do
+    subject {described_class.new(station)}
+
+    it 'has an entry station' do
+      expect(subject.entry_station).to eq station
+    end
+
+    it 'returns a penalty fare if no exit station given' do
+      expect(subject.penalty_fare).to eq Journey::PENALTY_FARE
+    end
+
+    context 'given an exit station' do
+      let(:other_station) { double :other_station}
+
+      before do
+        subject.finish(other_station)
+      end
+
+      it 'calculates a fare' do
+        expect(subject.fare).to eq 1
+      end
+
+      it 'knows if a journey is complete' do
+        expect(subject).to be_complete
+      end
+    end
+  end
 end
