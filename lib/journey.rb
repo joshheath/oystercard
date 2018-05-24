880090a7fe994f541c8ac07ad1ee9ce3a1c42d71
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-05-24 15:32:09 +0100

    ch.14 - halfway through writing and passing tests splitting out the journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,44 +1,16 @@
-require_relative 'oystercard'
-
 class Journey
-  attr_reader :balance
 
-  attr_accessor :entry_station
+PENALTY_FARE = 20
 
-  MAXIMUM_BALANCE = 90
-  MINIMUM_BALANCE = 1
-  MINIMUM_FARE = 2
+def complete?
+end
 
-  def initialize
-    @balance = 0
-    @entry_station = nil
-  end
-  
-  def in_journey?
-    !!entry_station
-  end
+def fare
+  PENALTY_FARE
+end
 
-  def touch_in(entry_station)
-    fail "balance is below £#{MINIMUM_BALANCE}" if @balance < MINIMUM_BALANCE
-    @entry_station = entry_station
-  end
-
-  def touch_out(exit_station)
-    deduct(MINIMUM_FARE)
-    @exit_station = exit_station
-    @entry_station = nil
-  end
-
-  def entry_station
-    @entry_station
-  end
-
-  def exit_station
-    @exit_station
-  end
-
-  def journeys
-    @journeys << journey
-  end
+def finish(station)
+  self
+end
 
 end
